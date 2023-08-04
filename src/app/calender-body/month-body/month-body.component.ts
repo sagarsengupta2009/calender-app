efca333e00ab37be1d89904a7d9d3d4c51629b9e
Sagar Sengupta <Sagar.Sengupta@symphonyretailai.com>
2023-08-04 18:02:02 +0530

    added unsubscription of all the subscriptions

--- src/app/calender-body/month-body/month-body.component.ts
+++ src/app/calender-body/month-body/month-body.component.ts
@@ -1,19 +1,21 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MonthBodyService } from './month-body.service';
 import { CalenderBodyService } from '../calender-body.service';
 import { ChangeMonthService } from '../change-month/change-month.service';
 import { HeaderService } from 'src/app/header/header.service';
 import { NgForm } from '@angular/forms';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-month-body',
   templateUrl: './month-body.component.html',
   styleUrls: ['./month-body.component.css'],
 })
-export class MonthBodyComponent implements OnInit {
+export class MonthBodyComponent implements OnInit,OnDestroy {
   @ViewChild('form') editEventForm!: NgForm;
   @Input() allEvents!: { id: string; title: string; date: string }[] | null;
+  private subscriptions: Subscription = new Subscription();
   dateCount!: number;
   year!: number;
   month!: any;
@@ -66,44 +68,50 @@ export class MonthBodyComponent implements OnInit {
       }
     }
 
-    this.changeMonthService.setDateOnChange.subscribe((monthYearObj) => {
-      this.year = monthYearObj.yearNumber;
-      monthIdx = monthYearObj.monthIdx;
-    });
+    this.subscriptions.add(
+      this.changeMonthService.setDateOnChange.subscribe((monthYearObj) => {
+        this.year = monthYearObj.yearNumber;
+        monthIdx = monthYearObj.monthIdx;
+      })
+    );
 
-    this.monthBodyService.dateCountChange.subscribe((dateMonthObj) => {
-      this.daysObj.length = 0;
-      this.daysObj = this.monthBodyService.getDays(dateMonthObj.dateCount);
-      for (const [i, event] of this.calenderBodyService.events.entries()) {
-        let dateArr = event.date.split('/');
-        let date = Number(dateArr[0]);
-        let month = Number(dateArr[1]);
-        let year = Number(dateArr[2]);
-        if (dateMonthObj.yearNumber === year) {
-          if (dateMonthObj.monthIdx + 1 === month) {
-            if (this.daysObj[date - 1]) {
-              this.daysObj[date - 1].events.push(event);
+    this.subscriptions.add(
+      this.monthBodyService.dateCountChange.subscribe((dateMonthObj) => {
+        this.daysObj.length = 0;
+        this.daysObj = this.monthBodyService.getDays(dateMonthObj.dateCount);
+        for (const [i, event] of this.calenderBodyService.events.entries()) {
+          let dateArr = event.date.split('/');
+          let date = Number(dateArr[0]);
+          let month = Number(dateArr[1]);
+          let year = Number(dateArr[2]);
+          if (dateMonthObj.yearNumber === year) {
+            if (dateMonthObj.monthIdx + 1 === month) {
+              if (this.daysObj[date - 1]) {
+                this.daysObj[date - 1].events.push(event);
+              }
             }
           }
         }
-      }
-    });
+      })
+    );
 
-    this.headerService.addEventToDate.subscribe((eventObj) => {
-      for (const [i, day] of this.daysObj.entries()) {
-        let dateArr = eventObj.date.split('/');
-        let date = Number(dateArr[0]);
-        let month = Number(dateArr[1]);
-        let year = Number(dateArr[2]);
-        if (this.year === year) {
-          if (monthIdx === month) {
-            if (day.day === date) {
-              day.events.push(eventObj);
+    this.subscriptions.add(
+      this.headerService.addEventToDate.subscribe((eventObj) => {
+        for (const [i, day] of this.daysObj.entries()) {
+          let dateArr = eventObj.date.split('/');
+          let date = Number(dateArr[0]);
+          let month = Number(dateArr[1]);
+          let year = Number(dateArr[2]);
+          if (this.year === year) {
+            if (monthIdx === month) {
+              if (day.day === date) {
+                day.events.push(eventObj);
+              }
             }
           }
         }
-      }
-    });
+      })
+    );
 
     this.closeContextMenu();
   }
@@ -253,12 +261,12 @@ export class MonthBodyComponent implements OnInit {
     this.closeContextMenu();
   }
 
-  drop(event: CdkDragDrop<{id: string, title: string, date: string}[]>) {
+  drop(event: CdkDragDrop<{ id: string; title: string; date: string }[]>) {
     let draggedEvent = event.previousContainer.data[event.currentIndex];
     let length = event.container.id.split('-').length;
     let element: any = event.container.element;
     let dropDate = Number(element.nativeElement.children[0].outerText);
-    this.daysObj[dropDate-1].events.push(draggedEvent);
+    this.daysObj[dropDate - 1].events.push(draggedEvent);
     for (const [i, day] of this.daysObj.entries()) {
       let dateArr = draggedEvent.date.split('/');
       let date = Number(dateArr[0]);
@@ -270,7 +278,10 @@ export class MonthBodyComponent implements OnInit {
     }
     this.calenderBodyService.changeEventDate(draggedEvent, dropDate);
     // moveItemInArray(this.allTem event.previousIndex, event.currentIndex);
-    // this.isListDirty = true;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
   // let dateArr = [
   //   {dateId: '', date: new Date(), events: [{id: '', title: '', date: ''}]}
